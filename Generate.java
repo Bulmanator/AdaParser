02838c979de703d9ffa7ee7d73f1bc0dcd3b5d69
James Bulman <bulmanator@gmail.com>
2018-03-11 03:51:47 +0000

    Working on if statements

--- Generate.java
+++ Generate.java
@@ -3,6 +3,7 @@ import java.util.Stack;
 
 public class Generate extends AbstractGenerate {
     public int indent;
+    public String filename;
     private Stack<String> nonterminals;
 
     public Generate() {
@@ -21,18 +22,20 @@ public class Generate extends AbstractGenerate {
     }
 
     public void commenceNonterminal(String nonterminal) {
+        for (int i = 0; i < indent; i++) { System.out.print(" "); }
         System.out.println("rggBEGIN " + nonterminal);
         nonterminals.push(nonterminal);
         indent += 4;
     }
 
     public void finishNonterminal(String nonterminal) {
+        indent -= 4;
+        for (int i = 0; i < indent; i++) { System.out.print(" "); }
         System.out.println("rggEND " + nonterminal);
         String last = nonterminals.pop();
         if (!last.equals(nonterminal)) {
             System.out.println("Error: Failed to end non-terminal (" + nonterminal + " != " + last + ")");
         }
-        indent -= 4;
     }
 
     public void reportSuccess() {
@@ -40,6 +43,13 @@ public class Generate extends AbstractGenerate {
         if (indent != 0) System.out.println("Incorrect indent!");
     }
 
-    public void reportError(Token token, String message) throws CompilationException {}
+    public void reportError(Token token, String message) throws CompilationException {
+        String final_message = filename + ":" + token.lineNumber + ": Error: " + message;
+        throw new CompilationException(final_message);
+    }
 
+    public void Reset() {
+        nonterminals.clear();
+        indent = 0;
+    }
 }
--- SyntaxAnalyser.java
+++ SyntaxAnalyser.java
@@ -2,12 +2,18 @@ import java.io.IOException;
 
 public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
 
+    private String filename;
+
     public SyntaxAnalyser(String filename) throws IOException {
-        myGenerate = new Generate();
+        this.filename = filename;
         lex = new LexicalAnalyser(filename);
     }
 
     public void _statementPart_() throws IOException, CompilationException {
+        myGenerate.filename = filename;
+        if (nextToken.symbol != Token.beginSymbol) {
+            myGenerate.reportError(nextToken, "\"begin\" symbol not found");
+        }
         while (true) {
             switch (nextToken.symbol) {
                 case Token.beginSymbol: {
@@ -22,14 +28,82 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
                     myGenerate.insertToken(nextToken);
                 }
                 break;
+                case Token.ifSymbol: {
+                    myGenerate.insertToken(nextToken);
+                    myGenerate.commenceNonterminal("IfStatement");
+                    IfStatement();
+                }
+                break;
             }
 
-            if (nextToken.symbol == Token.eofSymbol) {
+            if (nextToken.symbol == Token.endSymbol) {
                 break;
             }
 
             nextToken = lex.getNextToken();
         }
+    }
+
+    public void IfStatement() throws IOException, CompilationException {
+        Condition();
+        nextToken = lex.getNextToken();
+        if (nextToken.symbol != Token.thenSymbol) {
+            System.err.println(nextToken.toString());
+            myGenerate.reportError(nextToken, "\"then\" was expected after \"if\"");
+        }
+
+        myGenerate.insertToken(nextToken);
+        myGenerate.finishNonterminal("IfStatement");
+    }
+
+    public void Condition() throws IOException, CompilationException {
+        myGenerate.commenceNonterminal("Condition");
+        Token a = lex.getNextToken();
+        Token op = lex.getNextToken();
+        Token b = lex.getNextToken();
+
+        // The first identifier for the condition statement
+        if (a.symbol != Token.identifier) {
+            myGenerate.reportError(a, "identifier expected as leading part of conditional");
+        }
+        myGenerate.insertToken(a);
+
+        // @Todo(James): Do identifier checking!!!!
+
+        // The conditional operator
+        switch (op.symbol) {
+            case Token.greaterEqualSymbol:
+            case Token.greaterThanSymbol:
+            case Token.equalSymbol:
+            case Token.notEqualSymbol:
+            case Token.lessEqualSymbol:
+            case Token.lessThanSymbol: {
+                // Don't do anything as this is valid
+            }
+            break;
+            default: {
+                myGenerate.reportError(op, "\"" + op.text + "\" was not a valid conditional operator");
+            }
+            break;
+        }
+        myGenerate.insertToken(op);
+
+
+        // The second identifier for the condition statement
+        switch (b.symbol) {
+            case Token.identifier:
+            case Token.numberConstant:
+            case Token.stringConstant: {
+                // Once again don't do anything as this is valid!
+            }
+            break;
+            default: {
+                myGenerate.reportError(b, "expected identifier, number literal or string literal in conditional");
+            }
+            break;
+        }
+
+        myGenerate.finishNonterminal("Condition");
     }
 
     public void acceptTerminal(int symbol) throws IOException, CompilationException {}
