255a052b241552f3d7eae67527eff8a3e931c583
James <bulmanator@gmail.com>
2018-03-13 19:11:58 +0000

    Commented, pretty much done

--- Generate.java
+++ Generate.java
@@ -2,18 +2,32 @@ import java.io.IOException;
 import java.util.Stack;
 
 public class Generate extends AbstractGenerate {
-    public int indent;
-    public String filename;
+
+    // The amount to indent printing by
+    private int indent;
+    // The non-terminals currently in processing
     private Stack<String> nonterminals;
 
+    public String filename;
+    /**
+      Creates a new Generate instance
+     */
     public Generate() {
         indent = 0;
         nonterminals = new Stack<>();
     }
 
+    /**
+      Inserts the given token into the parse tree
+      @param token The token to insert into the parse tree
+     */
     public void insertToken(Token token) {
+        // Indents by the current indent amount
         for (int i = 0; i < indent; i++) { System.out.print(" "); }
+
         switch (token.symbol) {
+            // If the token is an identifier, number or string print out its value as well as its name
+            // and line number
             case Token.identifier:
             case Token.numberConstant:
             case Token.stringConstant: {
@@ -21,6 +35,7 @@ public class Generate extends AbstractGenerate {
                         + token.text + "' on line " + token.lineNumber);
             }
             break;
+            // Otherwise, just print the name of the token and the line number
             default: {
                 System.out.println("rggTOKEN " +
                         Token.getName(token.symbol) + " on line " + token.lineNumber);
@@ -29,50 +44,62 @@ public class Generate extends AbstractGenerate {
         }
     }
 
+    /**
+      Starts the given non-terminal and pushes it onto the process stack
+      @param nonterminal The non-terminal to begin
+     */
     public void commenceNonterminal(String nonterminal) {
+        // Indents by the current indent amount
         for (int i = 0; i < indent; i++) { System.out.print(" "); }
+
+        // Begins the non-terminal and pushes it to the processing stack
         System.out.println("rggBEGIN " + nonterminal);
         nonterminals.push(nonterminal);
+
+        // Increments the indent by 4 spaces
         indent += 4;
     }
 
+    /**
+      Finishes the given non-terminal and pops the top non-terminal from the processing stack<br/>
+      The non-terminal popped off the processing stack should be equal to the non-terminal given
+      @param nonterminal The non-terminal to finish
+     */
     public void finishNonterminal(String nonterminal) {
         indent -= 4;
         for (int i = 0; i < indent; i++) { System.out.print(" "); }
         System.out.println("rggEND " + nonterminal);
-        String last = nonterminals.pop();
-        if (!last.equals(nonterminal)) {
-            System.out.println("Error: Failed to end non-terminal (" + nonterminal + " != " + last + ")");
-        }
+        nonterminals.pop();
     }
 
+    /**
+      Reports success to the parser
+     */
     public void reportSuccess() {
         System.out.println("rggSUCCESS");
-        if (indent != 0) System.out.println("Incorrect indent!");
     }
 
+    /**
+      Reports an error to the parser, with the given message and the parse trace will be shown
+      @param token The token that caused the error
+      @param message An explanatory message about why the error occurred
+     */
     public void reportError(Token token, String message) throws CompilationException {
         String final_message = "";
-        /*if (token != null)
-            final_message = "\"Error: " + message + " at line " + token.lineNumber + " (" + filename + ")\"";
-        else
-            final_message = "\"Error: " + message + " (" + filename + ")\"";
-*/
+        // If the token isn't null add the line number the error occurred at to the error message
         if (token != null)
-            final_message = filename + ":" + token.lineNumber + ": \"Error: " + message + "\"";
+            final_message = filename + ":  \"Error: on line " + token.lineNumber + ", " + message + "\"";
         else
-            final_message = filename + ": \"Error: " + message + "\"";
+            final_message = "  \"Error: " + message + "\"";
 
+        // Pop all of the non-terminals currently in the processing stack and
+        // add them to the exception trace
         CompilationException trace = new CompilationException(final_message);
         while (!nonterminals.isEmpty()) {
-            trace = new CompilationException(nonterminals.pop(), trace);
+            trace = new CompilationException("parsing <" + nonterminals.pop() + ">", trace);
         }
 
+        // Throw the error
         throw trace;
-    }
-
-    public void Reset() {
-        nonterminals.clear();
-        indent = 0;
     }
 }
--- SyntaxAnalyser.java
+++ SyntaxAnalyser.java
@@ -4,34 +4,21 @@ import java.util.ArrayList;
 
 public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
 
-    public String filename;
-    // List of reserved words to prevent an identifier using them
-    private String[] reserved_words = {
-        "begin", "call", "do", "else", "end",
-        "float", "if", "integer", "is", "loop",
-        "procedure", "string", "then", "until",
-        "while", "for"
-    };
-
+    private String filename;
     /**
       Creates a new SyntaxAnalyser which will check the syntax of the file given
       @param filename The name of the file to analyse
     */
     public SyntaxAnalyser(String filename) throws IOException {
-        this.filename = filename;
         lex = new LexicalAnalyser(filename);
+        this.filename = filename;
     }
 
     /**
       Attepmts to parse the entire file catching and reporting any errors it encounters
     */
     public void _statementPart_() throws IOException, CompilationException {
-        // @Debug!!
-        {
-            String actual = filename.substring(filename.lastIndexOf(File.separator) + 1, filename.length());
-            myGenerate.filename = actual;
-        }
-
+        myGenerate.filename = filename;
         myGenerate.commenceNonterminal("StatementPart");
         // Accept the 'begin' symbol terminal
         acceptTerminal(Token.beginSymbol);
@@ -39,6 +26,7 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
         // Try to read a StatementList as it always follows a StatementPart
         StatementList();
 
+        // Finally, accept 'end' symbol to finish the parse
         acceptTerminal(Token.endSymbol);
         myGenerate.finishNonterminal("StatementPart");
     }
@@ -49,44 +37,50 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
     */
     public void StatementList() throws CompilationException, IOException {
         myGenerate.commenceNonterminal("StatementList");
+        // There is always at least one statement
         Statement();
         while (nextToken.symbol == Token.semicolonSymbol) {
+            // While a semi-colon is found after parsing a statement
+            // there must be another statement, so keep parsing
             acceptTerminal(Token.semicolonSymbol);
             Statement();
         }
         myGenerate.finishNonterminal("StatementList");
     }
 
+    /**
+      This will parse a single statement, one of six: if, while, do until, call, assignment and for
+     */
     private void Statement() throws CompilationException, IOException {
         myGenerate.commenceNonterminal("Statement");
         switch (nextToken.symbol) {
             case Token.identifier: {
-                // Must be an assignment statement
+                // Should try to parse an assignment statement
                 AssignmentStatement();
             }
             break;
             case Token.ifSymbol: {
-                // If statement!
+                // Should try to parse an if statement
                 IfStatement();
             }
             break;
             case Token.whileSymbol: {
-                // While loop!
+                // Should try to parse a while loop statement
                 WhileStatement();
             }
             break;
             case Token.doSymbol: {
-                // Do until loop!
+                // Should try to parse a do until loop statement
                 UntilStatement();
             }
             break;
             case Token.callSymbol: {
-                // Procedure call!
+                // Should try to parse a procedure statement
                 ProcedureStatement();
             }
             break;
             case Token.forSymbol: {
-                // For loop!
+                // Should try to parse a for loop statement
                 ForStatement();
             }
             break;
@@ -101,21 +95,33 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
     }
 
 
+    /**
+      This will attempt to parse an assigment statement, which can be a string constant or expression
+     */
     public void AssignmentStatement() throws IOException, CompilationException {
         myGenerate.commenceNonterminal("AssignmentStatement");
+        // An identifier must come first
         acceptTerminal(Token.identifier);
+        // Followed by a ':=' becomes symbol
         acceptTerminal(Token.becomesSymbol);
+
+        // If the available symbol is a string, then accept it
         if (nextToken.symbol == Token.stringConstant) {
             acceptTerminal(Token.stringConstant);
         }
         else {
+            // Otherwise try to parse an expression
             Expression();
         }
         myGenerate.finishNonterminal("AssignmentStatement");
     }
 
+    /**
+      This will attempt to parse an factor, which can be an identifier, number constant or bracketed expression
+     */
     public void Factor() throws CompilationException, IOException {
         myGenerate.commenceNonterminal("Factor");
+        // If the available symbol is an identifier or number constant, accept it and be done
         switch (nextToken.symbol) {
             case Token.identifier:
             case Token.numberConstant: {
@@ -123,12 +129,16 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
             }
             break;
             case Token.leftParenthesis: {
+                // If a left parenthesis is found then a bracketed expression must come next
                 acceptTerminal(Token.leftParenthesis);
                 Expression();
+                // After the expession has been parsed a closing right parenthesis must be accepted
                 acceptTerminal(Token.rightParenthesis);
             }
             break;
             default: {
+                // If neither one of the three, identifier, numberConstant or leftParenthesis
+                // then an error should be reported
                 myGenerate.reportError(nextToken, "unexpected symbol '"
                         + Token.getName(nextToken.symbol) +
                         "', expected identifier, numberConstant or (expression)");
@@ -138,21 +148,35 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
         myGenerate.finishNonterminal("Factor");
     }
 
+    /**
+      This will parse a term, which can be a factor, term * factor or term / factor
+     */
     public void Term() throws CompilationException, IOException {
         myGenerate.commenceNonterminal("Term");
+        // There will always be a factor to start with
         Factor();
+        // While there is a multiply or divide symbol available
         while (nextToken.symbol == Token.timesSymbol || nextToken.symbol == Token.divideSymbol) {
+            // Accept the multiply or divide
             acceptTerminal(nextToken.symbol);
+            // Try to parse another term
             Term();
         }
         myGenerate.finishNonterminal("Term");
     }
 
+    /**
+      This will parse an expression, which can be a term, expression + term or expression - term
+     */
     public void Expression() throws CompilationException, IOException {
         myGenerate.commenceNonterminal("Expression");
+        // There should always be a single term to start with
         Term();
+        // While there is a plus or minus symbol available
         while (nextToken.symbol == Token.plusSymbol || nextToken.symbol == Token.minusSymbol) {
+            // Accept the plus or minus symbol
             acceptTerminal(nextToken.symbol);
+            // Try to parse another term
             Term();
         }
         myGenerate.finishNonterminal("Expression");
@@ -163,17 +187,26 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
     */
     public void IfStatement() throws IOException, CompilationException {
         myGenerate.commenceNonterminal("IfStatement");
+        // Accept the 'if' symbol
         acceptTerminal(Token.ifSymbol);
+        // Then a condition must follow so try to parse
         Condition();
+        // If the condition successfully parses, then a 'then' symbol should follow
         acceptTerminal(Token.thenSymbol);
+        // The body of the if statment should contain a StatementList
         StatementList();
 
+        // Once the StatementList has been parsed, if the available symbol is else
         if (nextToken.symbol == Token.elseSymbol) {
+            // Accept the 'else' symbol
             acceptTerminal(Token.elseSymbol);
+            // Another StatementList should follow the else
             StatementList();
         }
 
+        // Accept the 'end' symbol
         acceptTerminal(Token.endSymbol);
+        // Accept the 'if' symbol
         acceptTerminal(Token.ifSymbol);
         myGenerate.finishNonterminal("IfStatement");
     }
@@ -183,17 +216,37 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
     */
     public void ForStatement() throws CompilationException, IOException {
         myGenerate.commenceNonterminal("ForStatement");
+        // Accept the 'for' symbol
         acceptTerminal(Token.forSymbol);
+        // A left parenthesis should follow, so accept it
         acceptTerminal(Token.leftParenthesis);
+
+        // The first section of the for loop declaration should be an assigmnet
+        // so try to parse one
         AssignmentStatement();
+        // After the assignment, a semi-colon should follow. Accept it
         acceptTerminal(Token.semicolonSymbol);
+
+        // The secdon section is a condition, so try to parse
         Condition();
+        // Once again, a semi-colon should follow. Accept it again
         acceptTerminal(Token.semicolonSymbol);
+
+        // The final section of the declaration is another assignment
+        // so try to parse
         AssignmentStatement();
+
+        // After this assignment, the closing right parenthesis should follow. Accept it
         acceptTerminal(Token.rightParenthesis);
+        // Then a 'do' symbol should follow. Accept this as well
         acceptTerminal(Token.doSymbol);
+
+        // The body of the for loop is a statement list, try to pase one
         StatementList();
+
+        // Finally to close an 'end' symbol should be found
         acceptTerminal(Token.endSymbol);
+        // followed by a 'loop' symbol
         acceptTerminal(Token.loopSymbol);
         myGenerate.finishNonterminal("ForStatement");
     }
@@ -203,11 +256,19 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
     */
     public void WhileStatement() throws CompilationException, IOException {
         myGenerate.commenceNonterminal("WhileStatement");
+        // Accept the 'while' symbol
         acceptTerminal(Token.whileSymbol);
+        // Then a condition should follow, so try to parse
         Condition();
+        // After the condition a 'loop' symbol should follow
         acceptTerminal(Token.loopSymbol);
+
+        // The body of the while loop is a StatementList, try to parse
         StatementList();
+
+        // Finally, an 'end' symbol should be present
         acceptTerminal(Token.endSymbol);
+        // Followed by a 'loop' symbol
         acceptTerminal(Token.loopSymbol);
         myGenerate.finishNonterminal("WhileStatement");
     }
@@ -217,9 +278,13 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
     */
     public void UntilStatement() throws CompilationException, IOException {
         myGenerate.commenceNonterminal("UntilStatement");
+        // Accept the 'do' symbol
         acceptTerminal(Token.doSymbol);
+        // The body of the do until loop is a StatementList, try to parse
         StatementList();
+        // After an 'until' symbol should be found, accept it
         acceptTerminal(Token.untilSymbol);
+        // The do until loop ends with a condition so try to parse
         Condition();
         myGenerate.finishNonterminal("UntilStatement");
     }
@@ -232,22 +297,23 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
      */
     public void Condition() throws IOException, CompilationException {
         myGenerate.commenceNonterminal("Condition");
+        // Accept the 'identifier' symbol
         acceptTerminal(Token.identifier);
+
+        // Should then be followed by a conditional operator
         ConditionalOperator();
+
+        // The available token could be one of three choices
         switch (nextToken.symbol) {
-            case Token.identifier: {
-                acceptTerminal(Token.identifier);
-            }
-            break;
-            case Token.numberConstant: {
-                acceptTerminal(Token.numberConstant);
-            }
-            break;
+            case Token.identifier:
+            case Token.numberConstant:
             case Token.stringConstant: {
-                acceptTerminal(Token.stringConstant);
+                // Was an identifier, numberConstant or stringConstant so accept
+                acceptTerminal(nextToken.symbol);
             }
             break;
             default: {
+                // Not one of the three report an error
                 myGenerate.reportError(nextToken, "unexpected symbol '"
                             + Token.getName(nextToken.symbol)
                             + "', expected identifier, numberConstant or stringConstant");
@@ -257,8 +323,13 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
         myGenerate.finishNonterminal("Condition");
     }
 
+    /**
+      Will attempt to parse a conditional operator, which can be one of: greaterThan, greaterEqual, equal,
+                lessThan, lessEqual
+     */
     private void ConditionalOperator() throws CompilationException, IOException {
         myGenerate.commenceNonterminal("ConditionalOperator");
+        // Check the available symbol for a conditional operator
         switch (nextToken.symbol) {
             case Token.greaterThanSymbol:
             case Token.greaterEqualSymbol:
@@ -266,51 +337,89 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
             case Token.notEqualSymbol:
             case Token.lessThanSymbol:
             case Token.lessEqualSymbol: {
+                // Is a conditional operator so accept it
                 acceptTerminal(nextToken.symbol);
             }
             break;
             default: {
+                // The available symbol was not a conditional operator so report an error
                 myGenerate.reportError(nextToken, "unexpected symbol '" + Token.getName(nextToken.symbol)
-                        + "', expected greaterThan, greaterEqual, equal, notEqual, lessThan or lessEqual");
+                        + "', expected >, >=, =, /=, < or <=");
             }
             break;
         }
+
         myGenerate.finishNonterminal("ConditionalOperator");
+
     }
 
+    /**
+      This will attempt to parse an procedure call
+     */
     public void ProcedureStatement() throws CompilationException, IOException {
         myGenerate.commenceNonterminal("ProcedureStatement");
+        // Expects a 'call' symbol to start with, accept it
         acceptTerminal(Token.callSymbol);
+        // Then an 'identifier' should follow the call so accept this as well
         acceptTerminal(Token.identifier);
+        // Next a opening left parenthesis should follow
         acceptTerminal(Token.leftParenthesis);
+
+        // The argument list comes between the opening and closing parentheses
+        // Try to parse the list
         ArgumentList();
+        // Finally, the closing right parenthesis should be the last thing to accept
         acceptTerminal(Token.rightParenthesis);
         myGenerate.finishNonterminal("ProcedureStatement");
     }
 
+    /**
+      This will attempt to parse an argument list
+     */
     public void ArgumentList() throws CompilationException, IOException {
         myGenerate.commenceNonterminal("ArgumentList");
+        // There must be an identifier in the argument list, so accept symbol
         acceptTerminal(Token.identifier);
+
+        // Keep accepting another identifier as long as there is a comma separating them
         while (nextToken.symbol == Token.commaSymbol) {
             acceptTerminal(Token.commaSymbol);
             acceptTerminal(Token.identifier);
         }
+
         myGenerate.finishNonterminal("ArgumentList");
     }
 
+    /**
+      This will try to accept the symbol given, if the nextToken does not contain this symbol
+      an error will be reported instead
+      @param symbol The symbol that the parser needs to accept
+     */
     public void acceptTerminal(int symbol) throws IOException, CompilationException {
+        // If nextToken is null then, the end of file has been reached and
+        // getNextToken() has been called again. Should never happen
         if (nextToken == null) {
             myGenerate.reportError(null, "unexpected end of file");
         }
         else if (nextToken.symbol == symbol) {
+            // If the symbol matches that of nextToken then insert it and move nextToken to the next available
             myGenerate.insertToken(nextToken);
             nextToken = lex.getNextToken();
         }
         else {
+            // If an error is reported and symbol is EOF, error with 'unexpected end of file' (looks nicer)
             if (nextToken.symbol == Token.eofSymbol) {
                 myGenerate.reportError(nextToken,
                         "unexpected end of file, expected " + Token.getName(symbol));
             }
+            else if (nextToken.symbol == Token.identifier) {
+                // If it is an identifier, error with the name of the identifier rather than IDENTIFIER
+                myGenerate.reportError(nextToken, "unexpected identifier '"
+                        + nextToken.text + "', expected " + Token.getName(symbol));
+            }
+
+            // Otherwise just report an error with the name of the symbol found
+            // and the name of the symbol expected
             myGenerate.reportError(nextToken, "unexpected symbol '"
                     + Token.getName(nextToken.symbol) + "', expected " + Token.getName(symbol));
         }
